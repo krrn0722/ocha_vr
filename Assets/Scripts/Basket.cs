b83b675479ea2797b5bd26a1bae5b73dd23f3e11
Kobayashi Yuhi <meikentarocheese@gmail.com>
2024-11-13 21:55:19 +0900

    お茶固定。
    爆発したら、籠が動かなくなる。

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -1,3 +1,102 @@
+// using System;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.Events;
+
+// public class Basket : MonoBehaviour
+// {
+//     [SerializeField] GameObject basket; // 籠のオブジェクト
+//     [SerializeField] int num_sheet = 0; // 現在のスコア
+//     [SerializeField] private GameObject[] cage_leafs; //籠に入れるお茶の葉
+//     private int current_cage_leaf_index = 0; //籠に入れるお茶の葉のインデックス
+
+//     private GameObject obj;
+//     private Rigidbody rb;
+
+//     private List<Collider> waitKinematicColliderList = new List<Collider>();
+
+//     int findedIndex;
+
+//     private void Start(){
+//         foreach(GameObject cage_leaf in cage_leafs){
+//             if(cage_leaf != null){
+//                 cage_leaf.SetActive(false);
+//             }
+//         }
+//     }
+
+    
+//      // お茶の葉が籠に入ったとき
+//     private void OnTriggerEnter(Collider other)
+//     {
+//         obj = other.gameObject;
+//         if (obj.CompareTag("Touched"))
+//         {
+            
+//             //rb = obj.GetComponent<Rigidbody>();
+            
+//             Debug.Log(other.gameObject.name + "がトリガーに入りました。");  
+//             // waitKinematicColliderList.Add(other);
+//             // obj.tag = "WaitKinematicLeaf";
+//             // StartCoroutine(DelayCoroutine(0.4f, () => {
+                
+//             //     // WaitLineamaticLeafのタグがついていれば
+//             //     if(obj.CompareTag("WaitKinematicLeaf"))
+//             //     {
+//             //         // キネマティックにして籠に入れる
+//             //         obj.transform.parent = basket.transform;
+//             //         other.enabled = false;
+//             //         rb.isKinematic = true;
+//             //         obj.tag = "NotMove";
+//             //         num_sheet += 1;
+//             //         Debug.Log("Tea Leaf entered! Score: " + num_sheet);
+//             //     }
+//             // }));
+
+//             Destroy(obj);
+
+//             num_sheet += 1;
+
+//             if(current_cage_leaf_index < cage_leafs.Length){
+//                 GameObject objToShow = cage_leafs[current_cage_leaf_index];
+//                 if(objToShow != null){
+//                     objToShow.SetActive(true);
+//                     current_cage_leaf_index++; //次のオブジェクトに進む
+//                 }
+//             }
+
+//         }
+//     }
+
+//     // private IEnumerator DelayCoroutine(float seconds, UnityAction callback)
+//     // {
+//     //     yield return new WaitForSeconds(seconds);
+//     //     callback?.Invoke();
+//     // }
+
+//     // お茶の葉が籠から出たとき
+//     // private void OnTriggerExit(Collider other)
+//     // {
+//     //     var obj = other.gameObject;
+//     //     if (obj.CompareTag("WaitKinematicLeaf"))
+//     //     {
+            
+//     //         other.gameObject.tag = "Touched";
+
+//     //         Debug.Log(other.gameObject.name + "がトリガーから出ました。");  
+//     //         Debug.Log("Tea Leaf exited! Score: " + num_sheet);
+//     //     }
+//     // }
+
+//     //スコアを計算すべき
+//     public int GetScore()
+//     {
+//         return num_sheet;
+//     }
+
+    
+// }
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -8,66 +107,73 @@ public class Basket : MonoBehaviour
 {
     [SerializeField] GameObject basket; // 籠のオブジェクト
     [SerializeField] int num_sheet = 0; // 現在のスコア
+    [SerializeField] private GameObject[] cage_leafs; //籠に入れるお茶の葉
+    private int current_cage_leaf_index = 0; //籠に入れるお茶の葉のインデックス
 
     private GameObject obj;
-    private Rigidbody rb;
 
-    private List<Collider> waitKinematicColliderList = new List<Collider>();
+    private void Start()
+    {
+        // ゲーム開始時に全てのオブジェクトを非表示にする
+        foreach(GameObject cage_leaf in cage_leafs)
+        {
+            if(cage_leaf != null)
+            {
+                cage_leaf.SetActive(false);
+            }
+        }
+    }
 
-    int findedIndex;
-     // お茶の葉が籠に入ったとき
+    // お茶の葉が籠に入ったとき
     private void OnTriggerEnter(Collider other)
     {
         obj = other.gameObject;
         if (obj.CompareTag("Touched"))
         {
-            
-            rb = obj.GetComponent<Rigidbody>();
-            
-            Debug.Log(other.gameObject.name + "がトリガーに入りました。");  
-            // waitKinematicColliderList.Add(other);
-            obj.tag = "WaitKinematicLeaf";
-            StartCoroutine(DelayCoroutine(0.4f, () => {
-                
-                // WaitLineamaticLeafのタグがついていれば
-                if(obj.CompareTag("WaitKinematicLeaf"))
+            Debug.Log(other.gameObject.name + "がトリガーに入りました。");
+
+            // 籠にお茶の葉を入れて、スコアを更新
+            num_sheet += 1;
+
+            // 次の非表示の葉を表示
+            if (current_cage_leaf_index < cage_leafs.Length)
+            {
+                GameObject objToShow = cage_leafs[current_cage_leaf_index];
+                objToShow.transform.parent = basket.transform;
+                if (objToShow != null)
                 {
-                    // キネマティックにして籠に入れる
-                    obj.transform.parent = basket.transform;
-                    other.enabled = false;
-                    rb.isKinematic = true;
-                    obj.tag = "NotMove";
-                    num_sheet += 1;
-                    Debug.Log("Tea Leaf entered! Score: " + num_sheet);
+                    Debug.Log("オブジェクトが現れるはず");
+                    objToShow.SetActive(true); // オブジェクトを表示
+                    objToShow.GetComponent<Collider>().enabled = false;
+                    objToShow.GetComponent<Rigidbody>().isKinematic = true;
+                    current_cage_leaf_index++; // 次のオブジェクトに進む
                 }
-            }));
+            }
+
+            Destroy(obj); // お茶の葉を削除
         }
-    }
-    private IEnumerator DelayCoroutine(float seconds, UnityAction callback)
-    {
-        yield return new WaitForSeconds(seconds);
-        callback?.Invoke();
     }
 
     // お茶の葉が籠から出たとき
     private void OnTriggerExit(Collider other)
     {
-        var obj = other.gameObject;
-        if (obj.CompareTag("WaitKinematicLeaf"))
+        if (other.CompareTag("NotTouch"))
         {
-            
-            other.gameObject.tag = "Touched";
+            Debug.Log(other.gameObject.name + "がトリガーから出ました。");
 
-            Debug.Log(other.gameObject.name + "がトリガーから出ました。");  
+            other.tag = "Touched";
+            
+            // 籠から出たお茶の葉を非表示にしてスコアを減らす
+            num_sheet -= 1;
+
+            // スコア更新後の処理など
             Debug.Log("Tea Leaf exited! Score: " + num_sheet);
         }
     }
 
-    //スコアを計算すべき
+    // スコアを取得
     public int GetScore()
     {
         return num_sheet;
     }
-
-    
 }
--- Assets/Scripts/LeafFall.cs
+++ Assets/Scripts/LeafFall.cs
@@ -1,56 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class LeafFall : MonoBehaviour
-{
-    [SerializeField] private Transform dish;
-    [SerializeField] private GameObject dishObj;
-    float coolTime = 0;
-    void Start()
-    {
+// public class LeafFall : MonoBehaviour
+// {
+//     [SerializeField] private Transform dish;
+//     [SerializeField] private GameObject dishObj;
+//     float coolTime = 0;
+//     void Start()
+//     {
         
-    }
+//     }
 
-    void Update()
-    {
-        // zのばあいんも　
-        //  0 < 230, 310 < 360 
-        if(180 < dish.eulerAngles.x && dish.eulerAngles.x < 220 || 320 < dish.eulerAngles.x && dish.eulerAngles.x < 360)
-        {
+//     void Update()
+//     {
+//         // zのばあいんも　
+//         //  0 < 230, 310 < 360 
+//         if(180 < dish.eulerAngles.x && dish.eulerAngles.x < 220 || 320 < dish.eulerAngles.x && dish.eulerAngles.x < 360)
+//         {
 
-            // Debug.Log("おちる");
-        }
-        // if()
+//             // Debug.Log("おちる");
+//         }
+//         // if()
 
-        // 0 < a <180 全部おちる
-        else if(0 < dish.eulerAngles.x && dish.eulerAngles.x < 180)
-        {
-            if(coolTime > 3)
-            {
-                var childCount = dish.childCount;
-                if(childCount > 0)
-                {
-                    Debug.Log(childCount+ "マイおちる");
-                    for (int i = 0; i < childCount; i++)
-                    {
-                        dishObj = dish.GetChild(i).gameObject;
-                        if(dishObj.tag == "NotMove")
-                        {
-                            dish.GetChild(i).parent = null;
+//         // 0 < a <180 全部おちる
+//         else if(0 < dish.eulerAngles.x && dish.eulerAngles.x < 180)
+//         {
+//             if(coolTime > 3)
+//             {
+//                 var childCount = dish.childCount;
+//                 if(childCount > 0)
+//                 {
+//                     Debug.Log(childCount+ "マイおちる");
+//                     for (int i = 0; i < childCount; i++)
+//                     {
+//                         dishObj = dish.GetChild(i).gameObject;
+//                         if(dishObj.tag == "NotMove")
+//                         {
+//                             dish.GetChild(i).parent = null;
                             
 
-                            dishObj.GetComponent<Collider>().enabled = true;
-                            dishObj.GetComponent<Rigidbody>().isKinematic = false;
-                        }
+//                             dishObj.GetComponent<Collider>().enabled = true;
+//                             dishObj.GetComponent<Rigidbody>().isKinematic = false;
+//                         }
 
-                    }
-                }
-                coolTime = 0;
-            }
+//                     }
+//                 }
+//                 coolTime = 0;
+//             }
 
-            // num_sheet = 0;
-        }
-        coolTime += Time.deltaTime;
-    }
-}
+//             // num_sheet = 0;
+//         }
+//         coolTime += Time.deltaTime;
+//     }
+// }
